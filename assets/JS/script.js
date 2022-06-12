63e302dea42f119ebd5bed0b4f9e20e178ab2816
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-06-12 15:52:49 +0000

    fixed js link bug

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,6 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
