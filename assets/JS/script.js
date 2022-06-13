60adc74e0254776f148cd6e9db8d9cfae7056fa1
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-06-13 10:32:15 +0000

    fixed addition bug

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -56,8 +56,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+ 
 }
 
 function displaySubtractQuestion() {
