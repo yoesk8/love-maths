4e07e8c732b5caeafbe52fff65ee322c7445b1b3
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-06-13 10:21:32 +0000

    Created rungame function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,9 +14,25 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    runGame("addition");
 });
 
-function runGame() {
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+ function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
