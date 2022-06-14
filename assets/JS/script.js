b529d624939bb53993802e9afb78ae9fd76e0f22
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-06-14 18:44:58 +0000

    Created update score and wrong answer functions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -52,8 +52,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -79,12 +81,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
 }
 
-function incrementScore() {
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+ function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
